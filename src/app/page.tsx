7049144295f1b694d717085b3679eee974dc4ee5
raelliflix-png <raelliflix@gmail.com>
2025-10-06 20:44:10 -0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+loccitane-landing/
+│
+├── package.json
+├── next.config.mjs
+├── postcss.config.mjs
+├── tailwind.config.ts
+├── tsconfig.json
+│
+└── src/
+    └── app/
+        ├── globals.css
+        └── page.tsx
