588fbf1600dfcd03b3248d3e47cd6830651f066e
raelliflix-png <raelliflix@gmail.com>
2025-10-06 21:01:59 -0300

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,5 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "L'Occitane by Israelli Barros",
-  description: "Descubra o universo de L'Occitane – beleza, natureza e bem-estar em cada detalhe.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
